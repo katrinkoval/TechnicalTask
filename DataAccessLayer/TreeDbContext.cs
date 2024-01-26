d41c03c906b180ab41bf155d9d8f2f0beb4453fa
Katerina <kovalkate21@gmail.com>
2024-01-26 11:27:56 +0200

    Add DataAccessLayer

---
+++ DataAccessLayer/TreeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class TreeDBContext : DbContext, ITreeDbContext
+    {
+        public TreeDBContext(DbContextOptions<TreeDBContext> options) : base(options) { }
+        public DbSet<Node> Nodes { get; set; }
+
+    }
+}
