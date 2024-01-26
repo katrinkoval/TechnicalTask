85dea0b6160c1fee8852424d8469317e7465c55f
Katerina <kovalkate21@gmail.com>
2024-01-27 00:20:46 +0200

    Add logic of converting from txt file

--- ConfigurationFileReader/TreeNode.cs
+++ ConfigurationFileReader/TreeNode.cs
@@ -8,6 +8,12 @@ namespace ConfigurationFileReader
 {
     public class TreeNode
     {
+        public TreeNode(string name) 
+        { 
+            Name = name;
+            Children = new List<TreeNode>();
+        }
+
         public string Name {  get; set; }
         public List<TreeNode> Children { get; set; }
     }
