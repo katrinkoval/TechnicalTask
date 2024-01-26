f63640bfa54e8ff4cb803c14568775c4a646c666
Katerina <kovalkate21@gmail.com>
2024-01-26 16:29:02 +0200

    Add SaveChanges method to interface ITreeDbContext

--- DataAccessLayer/ITreeDbContext.cs
+++ DataAccessLayer/ITreeDbContext.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    internal interface ITreeDbContext
+    public interface ITreeDbContext: IDisposable
     {
         DbSet<Node> Nodes { get; set; }
 
+        int SaveChanges();
     }
 }
