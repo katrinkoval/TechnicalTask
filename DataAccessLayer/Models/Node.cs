d2f9dc5b239c04d991919cdd2ade2d00b2d382ff
Katerina <kovalkate21@gmail.com>
2024-01-26 16:30:20 +0200

    Fix Node model

--- DataAccessLayer/Models/Node.cs
+++ DataAccessLayer/Models/Node.cs
@@ -9,7 +9,8 @@ namespace DataAccessLayer
     public class Node
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int ParentId { get; set; }
+        public string Name { get; set; }      
+        public int? ParentId { get; set; }     
+
     }
 }
